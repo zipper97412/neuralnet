965eb72cb34b3118017b1042341782efad44b0bc
zipper97412 <zipper97412@gmail.com>
2015-09-18 11:38:45 +0200

    rascasse

--- gride.go
+++ gride.go
@@ -12,8 +12,8 @@ type Gride struct {
     OutputLayer []Summer
     OutputsChan []chan float64
 }
-coucou
-sfsd func NewGride(layerLens []int) *Gride {
+
+func NewGride(layerLens []int) *Gride {
     
     G := Gride{}
     G.InputsChan = make([]chan float64, layerLens[0])
